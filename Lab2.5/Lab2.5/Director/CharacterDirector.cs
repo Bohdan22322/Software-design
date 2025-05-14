0384f7923ff80d071a2ed023567bab6c0f81018d
BohdanHnidyi <vtk241_gbs@student.ztu.edu.ua>
2025-05-14 16:53:09 +0300

    Add files via upload

---
+++ Lab2.5/Lab2.5/Director/CharacterDirector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class CharacterDirector
+{
+    public Character CreateHero(ICharacterBuilder builder)
+    {
+        return builder
+            .SetHeight(180)
+            .SetBodyType("Атлетичний")
+            .SetHairColor("Темне")
+            .SetEyeColor("Зелені")
+            .SetClothes("Броня героя")
+            .AddInventoryItem("Меч")
+            .AddDeed("Врятував село")
+            .Build();
+    }
+
+    public Character CreateEnemy(ICharacterBuilder builder)
+    {
+        return builder
+            .SetHeight(190)
+            .SetBodyType("Міцний")
+            .SetHairColor("Червоне")
+            .SetEyeColor("Чорні")
+            .SetClothes("Темна мантія")
+            .AddInventoryItem("Проклятий посох")
+            .AddDeed("Знищив місто")
+            .Build();
+    }
+}
+
